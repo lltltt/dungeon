a93e55bcf734dbaa7a63ecdf6838ce082ab58a4c
lltltt <lltltt@mailup.net>
2023-01-23 20:18:50 +0100

    better score

--- game.py
+++ game.py
@@ -3,12 +3,19 @@ import random
 life_count, life_icon = 3, '\u2665'
 medkit_count, medkit_icon = 2, '[+] '
 money_count, money_icon = 3, '$'
+score_count = 0
 
 def draw():
     global life_count
     if life_count == 0:
-        print("""
-        your dead!-_-
+        print(f""" ___________________________________________________
+      >_<    money: {money_count * money_icon}
+      -|-    life : {life_count * life_icon}
+      / \\    medkits: {medkit_count * medkit_icon}
+      you    score: {score_count}
+
+     you are dead. your score is : {score_count}
+___________________________________________________
 """)
         quit()      
 
@@ -16,12 +23,12 @@ def draw():
        I_I    money: {money_count * money_icon}
        -|-    life : {life_count * life_icon}
        / \\    medkits: {medkit_count * medkit_icon}
-       you  
+       you    score: {score_count}
 ___________________________________________________              
 """)
 
 def prompt():
-    global life_count, medkit_count, money_count
+    global life_count, medkit_count, money_count, score_count
 
     choice = input("""___________________
     What's next? 
@@ -37,6 +44,7 @@ ___________________
             print('ouch!Your shot!')
         if c == 0:
             money_count = money_count + 1
+            score_count = score_count + 1
             print('the enemi has missed.You have killed him!')
     if choice == 'b':
         if medkit_count == 0:
